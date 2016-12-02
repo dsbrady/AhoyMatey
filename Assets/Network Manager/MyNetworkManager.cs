8e9330946ef3a988cd39d6cecc4543ce4a7ca389
Scott Brady <sbrady@globalcloud.net>
2016-12-02 16:25:06 -0600

    Course Complete

---
+++ Assets/Network Manager/MyNetworkManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class MyNetworkManager : NetworkManager {
+
+	public void MyStartHost() {
+		Debug.Log(Time.timeSinceLevelLoad + ": Starting host");
+		StartHost();
+	}
+
+	public override void OnClientConnect(NetworkConnection conn) {
+		Debug.Log(Time.timeSinceLevelLoad + ": Client Connected to " + conn.address);
+	}
+
+	public override void OnStartClient(NetworkClient client) {
+		Debug.Log(Time.timeSinceLevelLoad + ": Client Start requested " );
+		InvokeRepeating("PrintDots", 0f, 1f);
+	}
+
+	public override void OnStartHost() {
+		Debug.Log(Time.timeSinceLevelLoad + ": Host started");
+		CancelInvoke();
+	}
+
+	void PrintDots() {
+		Debug.Log(".");
+	}
+}
---
+++ Assets/Players/PlayerNetworkBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using UnityStandardAssets.CrossPlatformInput;
+using System.Collections;
+
+public class PlayerNetworkBehaviour : NetworkBehaviour {
+
+	private Vector3 inputValue;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isLocalPlayer) {
+			return;
+		}
+
+		inputValue.x = CrossPlatformInputManager.GetAxis("Horizontal");
+		inputValue.y = 0f;
+		inputValue.z = CrossPlatformInputManager.GetAxis("Vertical");
+
+		transform.Translate(inputValue);
+	}
+
+	public override void OnStartLocalPlayer() {
+		GetComponentInChildren<Camera>().enabled = true;
+		GetComponentInChildren<AudioListener>().enabled = true;
+	}
+}
